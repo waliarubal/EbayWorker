a542abaefd295b8d4c6ac64c748766cd826db3af
Rubal Walia <rubal.walia@gmail.com>
2018-06-13 11:54:06 +0530

    Version updated.

--- EbayWorker/Properties/AssemblyInfo.cs
+++ EbayWorker/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.5.7.0")]
-[assembly: AssemblyFileVersion("18.5.7.0")]
+[assembly: AssemblyVersion("18.6.13.11")]
+[assembly: AssemblyFileVersion("18.6.13.11")]
